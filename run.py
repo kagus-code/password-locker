08e69e910e5214c7ddb93fddaf806e2ed7808156
Eston Kagwima <ekagwima745@gmail.com>
2021-04-11 01:06:02 +0300

    adding functions for credentials behaviour to run.py

--- run.py
+++ run.py
@@ -30,3 +30,41 @@ def check_existing_user(username):
     function to check if a user with that username exists and return Boolean
     '''
     return User.user_exist(username)
+
+    # end of user functions
+
+
+def create_credentials(account, username, password):
+    '''
+    function add credentials 
+    '''
+    new_credentials = Credentials(account, user_name, pass_word)
+    return new_credentials
+
+
+def save_credentials(credentials):
+    '''
+    function to save credentials
+    '''
+    return credentials.save_credentials()
+
+
+def delete_credentials(credentials):
+    '''
+    function to delete credentials
+    '''
+    return credentials.delete_credentials()
+
+
+def display_credentials():
+    '''
+    function that returns a list of all saved credentials 
+    '''
+    return Credentials.display_credentials()
+
+
+def generate_password():
+    '''
+    function to generate random password
+    '''
+    return Credentials.generate_password()
